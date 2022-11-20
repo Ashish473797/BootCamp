529bc0d3ecf4b288a0d4f21f69e44278c2eb5b02
Ashish473797 <ashish473797@gmail.com>
2022-11-20 12:44:02 +0530

    updated

---
+++ Assignment 34/7.cpp
@@ -0,0 +1,6 @@
+// There are 50 records in a file. Each record contains 6-character item-code, 20 characters for item-name and an integer price. 
+// Write a program to read these records, arrange them in the descending order of price and write them in the same file, 
+// overwriting the earlier records.
+#include<iostream>
+#include<fstream>
+#include<string>
